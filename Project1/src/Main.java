f8b9aaf891f73e25ce8e5e8d030ff3dd555b8180
Christina <christinacorkum89@gmail.com>
2026-01-08 15:38:16 -0400

    New Project-Christina

---
+++ Project1/src/Main.java
@@ -0,0 +1,16 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    //public static void main() method
+    //This is my first project in Java:
+    //Keep in your mind to capitalize the first letter of any word if
+    //it is a built-in class or user-defined class.
+
+    System.out.println("Welcome to our first project");
+    //Declaring new variables:
+    double salary = 18000.50;
+    double tax = 0.25;
+    double netSalary = salary * (1-tax);
+    System.out.println("The net Salary is: $" + netSalary);
+
+}
